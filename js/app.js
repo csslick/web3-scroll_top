1be0d0e381b909e6be540b1de2ea5e2dbc8c75d8
csslick <digitone@naver.com>
2023-12-13 16:10:03 +0900

    header 스크롤 액션

--- js/app.js
+++ js/app.js
@@ -3,13 +3,10 @@
 // 윈도우창이 스크롤 되는걸 감지
 window.addEventListener('scroll', function(){
     console.log(scrollY);
-
     /* 
-        일정 px 이상 스크롤 되면 top 보임
-        아니면 안보임(처음 상태, 최 상단일 때)
+        일정 px 이상 스크롤 되면 top 보임 아니면 안보임(처음 상태, 최 상단일 때)
     */
     const btn_top = document.querySelector('.btn-top');
-
     if(scrollY >= 1000) {
         // 버튼 보임
         btn_top.classList.add('show');
@@ -19,8 +16,14 @@ window.addEventListener('scroll', function(){
     }
 
     // header 영역 배경 바꾸기
+    const header = document.querySelector('header');
+    if(scrollY >= 10) {
+        // 헤더 배경 켜기
+        header.classList.add('show');
+    } else {
+        // 헤더 배경 끄기
+        header.classList.remove('show');
+    }
 
-    
-    
 })
 
